c061acd98d604d67218f82926202f6bf422052cf
henrique.almeida <henriquefr.almeida@gmail.com>
2023-10-01 13:15:28 -0300

    Add CardDigimonClientService for endpoint

--- src/main/java/br/dev/henriquealmeida/digimonwebapi/controller/DigimonController.java
+++ src/main/java/br/dev/henriquealmeida/digimonwebapi/controller/DigimonController.java
@@ -1,6 +1,8 @@
 package br.dev.henriquealmeida.digimonwebapi.controller;
 
+import br.dev.henriquealmeida.digimonwebapi.dto.response.CardResponse;
 import br.dev.henriquealmeida.digimonwebapi.dto.response.DigimonResponse;
+import br.dev.henriquealmeida.digimonwebapi.service.CardDigimonClientService;
 import br.dev.henriquealmeida.digimonwebapi.service.DigimonClientService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,6 +16,7 @@ import reactor.core.publisher.Mono;
 public class DigimonController {
 
     private DigimonClientService digimonClientService;
+    private CardDigimonClientService cardDigimonClientService;
 
     @GetMapping("/name/{name}")
     @ResponseStatus(HttpStatus.OK)
@@ -25,6 +28,12 @@ public class DigimonController {
     @ResponseStatus(HttpStatus.OK)
     public Flux<DigimonResponse> getDigimonsByLevel(@PathVariable String level) {
         return digimonClientService.findDigimonsByLevel(level);
+    }
+
+    @GetMapping("/card")
+    @ResponseStatus(HttpStatus.OK)
+    public Flux<CardResponse> getDigimonCardsByCardName(@RequestParam String name) {
+        return cardDigimonClientService.findCardByName(name);
     }
 
 
--- src/main/java/br/dev/henriquealmeida/digimonwebapi/service/DigimonClientService.java
+++ src/main/java/br/dev/henriquealmeida/digimonwebapi/service/DigimonClientService.java
@@ -2,7 +2,6 @@ package br.dev.henriquealmeida.digimonwebapi.service;
 
 import br.dev.henriquealmeida.digimonwebapi.dto.response.DigimonResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
@@ -13,9 +12,6 @@ import reactor.core.publisher.Mono;
 @Slf4j
 @Service
 public class DigimonClientService {
-
-    @Value("${baseUrl}")
-    private String BASE_URL;
 
     private final WebClient webClient;
 
